edcea099895b94214f23e69dcf8b5d2c17056284
gaurav.parange <gaurav.parange@pmechtech.com>
2024-09-10 19:13:48 +0530

    changes

---
+++ BookStore/BookStore/Controllers/BookController.cs
@@ -0,0 +1,24 @@
+﻿using BookStore.Models;
+using BookStore.Repositary;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepositary _bookRepositary;
+        public BookController()
+        {
+            _bookRepositary = new BookRepositary();
+        }
+        public List<BookModel> GetAllBooks()
+        {
+            return _bookRepositary.GetAllBooks();
+        }
+        public List<BookModel> SearchBooks(string title,string Authorname)
+        {
+            return _bookRepositary.SearchBook(title, Authorname);
+        }
+    }
+}
---
+++ BookStore/BookStore/Repositary/BookRepositary.cs
@@ -0,0 +1,35 @@
+﻿using BookStore.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+
+namespace BookStore.Repositary
+{
+    public class BookRepositary
+    {
+        public List<BookModel> GetAllBooks()
+        {
+            return Datasource();
+        }
+        public BookModel GetAllBookById(int id)
+        {
+            return Datasource().Where( x => x.Id == id).FirstOrDefault();
+        }
+        public List<BookModel> SearchBook(string title, string authorName)
+        {
+            return Datasource().Where(x => x.Title.Contains(title) && x.Author.Contains(authorName)).ToList();
+
+        }
+        private List<BookModel> Datasource()
+        {
+            return new List<BookModel>()
+           {
+               new BookModel() {Id = 1, Title = "MVC", Author = "Gaurav" },
+               new BookModel() {Id = 2, Title = "Dot Net Core", Author = "Gaurav" },
+               new BookModel() {Id = 3, Title = "C#", Author = "Shifa" },
+               new BookModel() {Id = 4, Title = "Java", Author = "Suraj" },
+               new BookModel() {Id = 5, Title = "Php", Author = "Shekhar" },
+            };
+        }
+    }
+}
